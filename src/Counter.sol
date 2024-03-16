d813fdc1ac2fec0bc3eee90d6d438c30fc79d1cc
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-03-16 16:04:52 +0100

    feat(EntryPoint.sol)

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+/* CCIP */
+contract EntryPoint {
+    
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
 }
