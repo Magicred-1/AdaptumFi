1afe49b1e2a24073e20ffe29ad54e777b4a0edbb
Magicred-1 <djason.gadiou.sio@gmail.com>
2024-03-16 02:15:10 +0000

    Fixing Footer ooppppssss ...

--- adaptum/src/_components/footer.tsx
+++ adaptum/src/_components/footer.tsx
@@ -4,13 +4,13 @@ import logo from '../assets/logos/logo.png'
 const Footer = () => {
   return (
     <div className="flex items-center justify-between text-white bg-gray-900 px-4 md:px-10 py-4 border-t border-gray-700">
-    <div className="flex items-center md:gap-2">
-        <Image src={logo} alt="logo" width={50} height={50} className='w-8 md:w-12' />
-        <span className='text-sm md:text-base'>AdaptumFi</span>
-    </div>
-    <span className='text-xs md:text-sm'>
-    © 2024 AdaptumFi. All right reserved
-    </span>
+        <div className="flex items-center md:gap-2">
+            <Image src={logo} alt="logo" width={50} height={50} className='w-8 md:w-12' />
+            <span className='text-sm md:text-base'>AdaptumFi</span>
+        </div>
+        <span className='text-xs md:text-sm'>
+        © 2024 AdaptumFi. All right reserved
+        </span>
     </div>
   )
 }
