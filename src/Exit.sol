d0000947747f67f07af7c0f5f6e14c57e82155e6
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-03-17 00:26:03 +0100

    fix

--- src/Exit.sol
+++ src/Exit.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IHypNative} from "lib/hyperlane-monorepo/solidity/contracts/token/HypNative.sol";
+import {IHypNative} from "lib/hyperlane-monorepo/typescript/token/contracts/HypNative.sol";
 
 contract ExitPoint is IHypNative {
-    
+
 }
