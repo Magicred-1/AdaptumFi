36461ac81609cac55b2c2f96609fb2c7e38c3000
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-03-17 02:50:06 +0100

    commit

--- src/Exit.sol
+++ src/Exit.sol
@@ -1,8 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IHypNative} from "lib/hyperlane-monorepo/typescript/token/contracts/HypNative.sol";
+import {HypERC20Collateral} from "lib/hyperlane-monorepo/solidity/contracts/token/HypERC20Collateral.sol";
 
-contract ExitPoint is IHypNative {
+contract ExitPoint {
+
+    HypERC20Collateral hype20Collat;
+
+    constructor(address _hype20Collat) {
+        hype20Collat = HypERC20Collateral(payable(_hype20Collat));
+    }
+
+    function sendBackTokens(address _token, uint256 amount, bytes calldata data){
+        IERC20 token = IERC20(_token);
+        token.transferFrom(msg.sender, amount);
+        /* TODO: Wrap route + message */
+        
+    }
 
 }
