50b11a6e9b3f743f05755d57bc5f56e87f4cc5b6
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2024-03-17 05:07:46 +0100

    feat(CallbackPoint)

--- src/Exit.sol
+++ src/Exit.sol
@@ -2,20 +2,32 @@
 pragma solidity ^0.8.13;
 
 import {HypERC20Collateral} from "lib/hyperlane-monorepo/solidity/contracts/token/HypERC20Collateral.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IMailbox} from "lib/hyperlane-monorepo/solidity/contracts/interfaces/IMailbox.sol";
 
 contract ExitPoint {
 
     HypERC20Collateral hype20Collat;
+    IMailbox mailBox;
+    uint32 destinationChain;
 
-    constructor(address _hype20Collat) {
+    constructor(address _mailbox, address _hype20Collat, uint32 _destinationChain) {
         hype20Collat = HypERC20Collateral(payable(_hype20Collat));
+        mailBox = IMailbox(_mailbox);
+        destinationChain = _destinationChain;
     }
 
-    function sendBackTokens(address _token, uint256 amount, bytes calldata data){
+    function sendBackTokens(address _token, uint256 amount, address _target, bytes calldata data) external payable {
         IERC20 token = IERC20(_token);
-        token.transferFrom(msg.sender, amount);
-        /* TODO: Wrap route + message */
-        
+        token.transferFrom(msg.sender, address(this), amount);
+        /* message */
+        bytes32 recipientAddress = bytes32(uint256(uint160(_target)) << 96);
+        mailBox.dispatch(destinationChain, recipientAddress, data);
+        /* warp route */
+        hype20Collat.transferRemote(
+            destinationChain,
+            recipientAddress,
+            amount);
     }
 
 }
